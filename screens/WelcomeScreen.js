c5ff7c9d70da085552a70deab3a318ad112cd569
Chashpong <chashpong@example.com>
2025-05-17 14:57:53 +0700

    17/5

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -14,10 +14,7 @@ export default function WelcomeScreen({ navigation }) {
         <Text style={styles.buttonText}>REGISTER</Text>
       </TouchableOpacity>
 
-      {/* ปุ่มไปหน้า Home */}
-      <TouchableOpacity style={styles.buttonHome} onPress={() => navigation.navigate('HomeScreen')}>
-        <Text style={styles.buttonHomeText}>GO TO HOME</Text>
-      </TouchableOpacity>
+      
     
 
       
